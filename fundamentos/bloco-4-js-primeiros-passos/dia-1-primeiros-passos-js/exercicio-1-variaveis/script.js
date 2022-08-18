857637285978f0bfe5c4a1a752be2a92a4a927e3
Vitória <vitcs11@gmail.com>
2022-08-18 12:59:09 -0500

    if/else

---
+++ fundamentos/bloco-4-js-primeiros-passos/dia-1-primeiros-passos-js/exercicio-1-variaveis/script.js
@@ -0,0 +1,11 @@
+const notaJoana = 50;
+
+if (notaJoana >= 80) {
+    console.log ("Parabéns, você foi aprovado!")
+} 
+else if (notaJoana >= 60 && notaJoana <80) {
+    console.log("Você está em nossa lista de espera.")
+}
+else {
+    console.log("Você foi reprovado.")
+}
