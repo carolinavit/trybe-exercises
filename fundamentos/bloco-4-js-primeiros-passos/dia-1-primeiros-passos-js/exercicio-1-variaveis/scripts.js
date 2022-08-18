31a65d1916a506c78d9f12ed68d67203863e4920
Vitória <vitcs11@gmail.com>
2022-08-18 13:47:29 -0500

    Exercicio switch/case

---
+++ fundamentos/bloco-4-js-primeiros-passos/dia-1-primeiros-passos-js/exercicio-1-variaveis/scripts.js
@@ -0,0 +1,16 @@
+let candidato= "aprovado";
+
+switch (candidato) {
+    case "aprovado":
+    console.log("Parabéns, você foi aprovado!");
+    break;
+    case "lista":
+    console.log("Você está na lista de espera.");
+    break;
+    case "reprovado":
+    console.log("Você foi reprovado.");
+    break;
+    default: 
+    console.log("não se aplica.");
+
+}
