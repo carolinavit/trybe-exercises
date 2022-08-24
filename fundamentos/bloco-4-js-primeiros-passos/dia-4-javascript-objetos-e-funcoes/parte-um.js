03acc30165087f03c588a2477f8e577f1eeb5383
Vitória <vitcs11@gmail.com>
2022-08-24 02:05:03 -0300

    soluciona exercicios de objetos

--- fundamentos/bloco-4-js-primeiros-passos/dia-4-javascript-objetos-e-funcoes/parte-um.js
+++ fundamentos/bloco-4-js-primeiros-passos/dia-4-javascript-objetos-e-funcoes/parte-um.js
@@ -1,34 +1,104 @@
 //1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console:
 
-let info = {
+/* let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
 
+console.log("Boas vindas, " + info.personagem + "!");
+
 // 🚀 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. 
 
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim'
+};
+console.table(info); 
 
 
 // 3 - Faça um for/in que mostre todas as chaves do objeto. 
 
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim'
+};
+
+for (let key in info) {
+  console.log(key);
+}
+
+// 4 - Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto.
+
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  recorrente: "Sim",
+};
 
 
-// 4 - Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. 
+for (let key in info) {
+  console.log(info[key]);
+} 
 
+// 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves.
 
-
-// 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. 
-
+let info2 = {
+  personagem: "Tio Patinhas",
+  origem: ["Christmas on Bear Mountain", "Dell's Four Color Comics #178"],
+  nota: "O último MacPatinhas",
+  recorrente: "Sim",
+};
+for (let key in info) {
+  if (key === "recorrente" && info[key] === "Sim" && info2[key] === "Sim") {
+    console.log("Ambos recorrentes");
+  } else {
+    console.log(info[key] + " e " + info2[key]);
+  }
+} */
 
 // 🚀 6 - Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
 
-// 🚀 7 - Adicione um novo livro favorito na chave livrosFavoritos, que é um array. Atribua a esta chave um objeto contendo as seguintes informações: 
+let leitor = {
+  nome: "Julia",
+  sobrenome: "Pessoa",
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: "O Pior Dia de Todos",
+      autor: "Daniela Kopsch",
+      editora: "Tordesilhas",
+    },
+  ],
+};
 
-{
-  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
-  autor: 'JK Rowling',
-  editora: 'Rocco',
-}
+console.log(
+  "O livro favorito de " +
+    leitor.nome +
+    " " +
+    leitor.sobrenome +
+    " se chama " +
+    leitor.livrosFavoritos[0].titulo
+);
+
+// 🚀 7 - Adicione um novo livro favorito na chave livrosFavoritos, que é um array. Atribua a esta chave um objeto contendo as seguintes informações:
+
+leitor.livrosFavoritos.push({
+  titulo: "Harry Potter e o Prisioneiro de Azkaban",
+  autor: "JK Rowling",
+  editor: "Rocco",
+});
 
 // 🚀 8 - Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: "Julia tem 2 livros favoritos".
+
+console.log(
+  leitor["nome"] +
+    " tem " +
+    leitor["livrosFavoritos"].length +
+    " livros favoritos."
+);
