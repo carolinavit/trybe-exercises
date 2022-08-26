3382537e93f52abfa1aed80099b467ed12af25a8
Vitória <vitcs11@gmail.com>
2022-08-26 00:41:53 -0300

    conclui exercicios

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/exercicios-fixacao/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/exercicios-fixacao/script.js
@@ -8,3 +8,15 @@ let emergencyTasksHeaders = document.querySelectorAll(".emergency-tasks h3");
 for (let index = 0; index < emergencyTasksHeaders.length; index += 1) {
   emergencyTasksHeaders[index].style.backgroundColor = "purple";
 }
+let noEmergencyTasks = document.querySelector(".no-emergency-tasks");
+noEmergencyTasks.style.backgroundColor = "yellow";
+
+let noEmergencyTasksHeaders = document.querySelectorAll(
+  ".no-emergency-tasks h3"
+);
+for (let index = 0; index < noEmergencyTasksHeaders.length; index += 1) {
+  noEmergencyTasksHeaders[index].style.backgroundColor = "black";
+}
+
+const footer = document.getElementById("footer-container");
+footer.style.backgroundColor = "green";
